c3c4cd0090e91790136cc5c7e77315f509bc9899
Raul Metsma <raul@metsma.ee>
2012-10-14 10:32:48 +0000

    add man pages
    Ticket: #19782
    
    
    git-svn-id: https://svn.eesti.ee/projektid/idkaart/qdigidoc/trunk@7490 c45a0044-e149-41c6-9d26-0409f48354e9

---
+++ client/qdigidocclient.1.cmake
@@ -0,0 +1,8 @@
+.\" Manpage for qdigidocclient
+.TH qdigidocclient 1 "${BUILD_DATE}" "${VERSION}" "qdigidocclient man page"
+.SH NAME
+qdigidocclient \- Qt based UI application for verifying and signing digital signatures
+.SH SYNOPSIS
+qdigidocclient [FILES]
+.SH SEE ALSO
+cdigidoc(1), digidoc-tool(1), qdigidoccrypto(1), qesteidutil(1)
