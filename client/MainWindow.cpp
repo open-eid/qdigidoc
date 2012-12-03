ab1783a3d8719341afce72e50131739c7b25d303
Raul Metsma <raul@metsma.ee>
2012-12-03 18:03:09 +0000

    dont hide crypto button
    Ticket: #21162
    
    
    git-svn-id: https://svn.eesti.ee/projektid/idkaart/qdigidoc/trunk@7991 c45a0044-e149-41c6-9d26-0409f48354e9

--- client/MainWindow.cpp
+++ client/MainWindow.cpp
@@ -86,9 +86,9 @@ MainWindow::MainWindow( QWidget *parent )
 	buttonGroup->setId( homeSign, HomeSign );
 	buttonGroup->setId( homeView, HomeView );
 	buttonGroup->setId( homeCrypt, HomeCrypt );
-#ifdef APPSTORE
+//#ifdef APPSTORE
 	homeCrypt->hide();
-#endif
+//#endif
 
 	buttonGroup->setId( signAddFile, SignAdd );
 
