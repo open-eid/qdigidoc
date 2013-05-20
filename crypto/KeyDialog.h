bcd729712a816dc8a73529b81e12edb99ceb3286
Raul Metsma <raul@metsma.ee>
2013-05-20 22:41:11 +0000

    Update years
    git-svn-id: https://svn.eesti.ee/projektid/idkaart/qdigidoc/trunk@9469 c45a0044-e149-41c6-9d26-0409f48354e9

--- crypto/KeyDialog.h
+++ crypto/KeyDialog.h
@@ -1,8 +1,8 @@
 /*
  * QDigiDocCrypto
  *
- * Copyright (C) 2009-2012 Jargo Kõster <jargo@innovaatik.ee>
- * Copyright (C) 2009-2012 Raul Metsma <raul@innovaatik.ee>
+ * Copyright (C) 2009-2013 Jargo Kõster <jargo@innovaatik.ee>
+ * Copyright (C) 2009-2013 Raul Metsma <raul@innovaatik.ee>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
