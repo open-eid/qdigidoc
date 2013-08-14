48e2475fb0fac3c623d870b0466d3a7fee8bf4e7
Raul Metsma <raul@metsma.ee>
2013-08-14 12:28:32 +0000

    launch crypto instead
    Ticket: #22937
    
    
    git-svn-id: https://svn.eesti.ee/projektid/idkaart/qdigidoc/trunk@9739 c45a0044-e149-41c6-9d26-0409f48354e9

--- crypto/qdigidoccrypto.1.cmake
+++
@@ -1,8 +0,0 @@
-.\" Manpage for qdigidoccrypto
-.TH qdigidoccrypto 1 "${BUILD_DATE}" "${VERSION}" "qdigidoccrypto man page"
-.SH NAME
-qdigidoccrypto \- Qt based UI application for encrypting and decrypting documents
-.SH SYNOPSIS
-qdigidoccrypto [FILES]
-.SH SEE ALSO
-cdigidoc(1), digidoc-tool(1), qdigidocclient(1), qesteidutil(1)
