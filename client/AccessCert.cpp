4fd66311956357dc95082f15cfe021b98eda04a9
Raul Metsma <raul@metsma.ee>
2014-01-08 21:43:44 +0000

    no need use ACL
    Ticket: #23433
    
    
    git-svn-id: https://svn.eesti.ee/projektid/idkaart/qdigidoc/trunk@11298 c45a0044-e149-41c6-9d26-0409f48354e9

--- client/AccessCert.cpp
+++ client/AccessCert.cpp
@@ -277,7 +277,7 @@ bool AccessCert::installCert( const QByteArray &data, const QString &password )
 	SecKeyImportExportParameters params;
 	memset( &params, 0, sizeof(params) );
 	params.version = SEC_KEY_IMPORT_EXPORT_PARAMS_VERSION;
-	params.flags = kSecKeyImportOnlyOne;
+	params.flags = kSecKeyImportOnlyOne|kSecKeyNoAccessControl;
 	params.keyAttributes = CSSM_KEYATTR_PERMANENT|CSSM_KEYATTR_EXTRACTABLE;
 	params.passphrase = CFStringCreateWithCharacters( 0,
 		reinterpret_cast<const UniChar *>(password.unicode()), password.length() );
