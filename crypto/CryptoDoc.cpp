53c827c2bf5e2977ee2cfc8e852c49ac8d055ba9
Raul Metsma <raul@metsma.ee>
2017-12-07 13:26:24 +0200

    QMultiHash prepends items
    IB-5049
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- crypto/CryptoDoc.cpp
+++ crypto/CryptoDoc.cpp
@@ -584,8 +584,8 @@ void CryptoDocPrivate::writeCDoc(QIODevice *cdoc, const QByteArray &transportKey
 	props.insert("DocumentFormat", "ENCDOC-XML|" + ver);
 	props.insert("LibraryVersion", qApp->applicationName() + "|" + qApp->applicationVersion());
 	props.insert("Filename", file);
-	for(const File &file: qAsConst(files))
-		props.insertMulti("orig_file", QString("%1|%2|%3|%4").arg(file.name).arg(file.data.size()).arg(file.mime).arg(file.id));
+	for(QList<File>::const_reverse_iterator i = files.crbegin(), end = files.crend(); i != end; ++i)
+		props.insert("orig_file", QString("%1|%2|%3|%4").arg(i->name).arg(i->data.size()).arg(i->mime).arg(i->id));
 
 	QXmlStreamWriter w(cdoc);
 	w.setAutoFormatting(true);
